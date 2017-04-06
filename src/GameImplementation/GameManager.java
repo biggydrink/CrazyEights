eb1e6b8264ecdd296e12514f29f6c9352e0f0332
biggydrink <andrew.butz@gmail.com>
2017-04-06 09:59:51 -0500

    Modified how scores are shown after a game

--- src/GameImplementation/GameManager.java
+++ src/GameImplementation/GameManager.java
@@ -18,9 +18,11 @@ public class GameManager {
 
 
         boolean play = true;
+        int gamesPlayed = 0;
         while (play) {
             Game game = new Game(playerList,gInterface);
             game.playGame();
+            gamesPlayed++;
             System.out.println("Good game!");
             readPlayerScores(playerList);
             System.out.println("Play again?");
@@ -42,8 +44,8 @@ public class GameManager {
     public static void readPlayerScores(LinkedList<Player> playerList) {
         for (Player player : playerList) {
             System.out.println(player.getName() + "'s score is " + player.getScore());
-            System.out  .println("");
         }
+        System.out.println("");
         for (Player player : playerList) {
             readPlayerScoresDetailed(player);
         }
@@ -55,13 +57,17 @@ public class GameManager {
      */
     public static void readPlayerScoresDetailed(Player player) {
         System.out.println(player.getName() + "'s detailed score:");
-        System.out.println("Points played last game: " + player.getPointsPlayedThisGame());
-        System.out.println("Cards played last game: " + player.getCardsPlayedThisGame());
-        System.out.println("Score this game: " + player.getScoreThisGame());
-        System.out.println("-");
-        System.out.println("Total Score: " + player.getScore());
-        System.out.println("Total cards played: " + player.getCardsPlayed());
+        System.out.println("");
+        // If played more than one game, show last game stats separately
+        if (player.getPointsPlayedThisGame() != player.getPointsPlayed()) {
+            System.out.println("Points played last game: " + player.getPointsPlayedThisGame());
+            System.out.println("Cards played last game: " + player.getCardsPlayedThisGame());
+            System.out.println("Score this game: " + player.getScoreThisGame());
+            System.out.println("-");
+        }
         System.out.println("Total points played: " + player.getPointsPlayed());
+        System.out.println("Total cards played: " + player.getCardsPlayed());
+        System.out.println("Total Score: " + player.getScore());
         System.out.println("");
 
     }
